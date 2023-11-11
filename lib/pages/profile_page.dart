1549b4234a5e855bbfe93e2ecf482cd98fdcb3cb
noa-goldenberg <noagold8@gmail.com>
2023-11-11 22:54:18 +0200

    profile page: profile pic and name, age, country

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,6 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:myfirstapp/services/helpers.dart';
+
+// todo - export to User class
 
 class ProfilePage extends StatelessWidget {
   ProfilePage({super.key});
@@ -44,9 +48,113 @@ class ProfileData extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var age = calcAge(DateTime.fromMillisecondsSinceEpoch(
+        userData['birth_date'].millisecondsSinceEpoch));
+    var firstName = userData['first_name'];
+    var username = userData['username'];
+    var country = userData['country'];
+    var bio = userData['bio'];
+    var imagePath =
+        'https://images.unsplash.com/photo-1554151228-14d9def656e4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=333&q=80';
+
     return Scaffold(
-      body:
-          Center(child: Text("PROFILE first name: ${userData['first_name']}")),
+      body: ListView(
+        physics: BouncingScrollPhysics(),
+        children: [
+          ProfileWidget(
+            imagePath: imagePath,
+            onClicked: () async {},
+            firstName: firstName,
+            age: age,
+            country: country,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ProfileWidget extends StatelessWidget {
+  final String imagePath;
+  final VoidCallback onClicked;
+  final String firstName;
+  final int age;
+  final String country;
+
+  const ProfileWidget(
+      {Key? key,
+      required this.imagePath,
+      required this.onClicked,
+      required this.firstName,
+      required this.age,
+      required this.country})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(20, 10, 40, 0),
+          child: Stack(children: [
+            buildImage(),
+            Positioned(
+              child: buildIcon(color: const Color.fromARGB(255, 156, 204, 101)),
+              bottom: 0,
+              right: 4,
+            ),
+          ]),
+        ),
+        Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Text(
+                "${firstName}, 23",
+                style: TextStyle(fontSize: 30),
+              ),
+            ),
+            Text(
+              "Israel",
+              style: TextStyle(fontSize: 18),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget buildIcon({required Color color}) {
+    return buildCircle(
+      color: Colors.white,
+      all: 3.0,
+      child: buildCircle(
+          color: color,
+          child: Icon(Icons.edit, color: Colors.white, size: 20),
+          all: 8.0),
+    );
+  }
+
+  Widget buildCircle(
+      {required Widget child, required Color color, required double all}) {
+    return ClipOval(
+      child: Container(
+        padding: EdgeInsets.all(all),
+        color: color,
+        child: child,
+      ),
+    );
+  }
+
+  Widget buildImage() {
+    final image = NetworkImage(imagePath);
+
+    return ClipOval(
+      child: Material(
+        color: Colors.transparent, // why?
+        child:
+            Ink.image(image: image, fit: BoxFit.cover, width: 128, height: 128),
+      ),
     );
   }
 }
