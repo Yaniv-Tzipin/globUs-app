f3a488d15d315b6973aca34777b371509ac5048f
Yaniv-Tzipin <yaniv8985@gmail.com>
2023-11-17 13:43:14 +0200

    fixing the birthdate issue

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -10,7 +10,7 @@ class UserProfile{
   UserProfile(Map<String, dynamic> userData){
     username = userData['username'];
     //!!! to talk with Noa about this 
-    age = 23; //calcAge(DateTime.fromMillisecondsSinceEpoch(userData['birth_date'].millisecondsSinceEpoch));
+    age = calcAge(DateTime.fromMillisecondsSinceEpoch(userData['birth_date'].millisecondsSinceEpoch));
     originCountry = userData['country'];
     bio = userData['bio'];
     profileImagePath = userData['profile_image'] ?? "";
