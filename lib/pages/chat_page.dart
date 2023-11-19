60ef16328e87c00b4e753493a1b25fe1c2876cfd
Yaniv-Tzipin <yaniv8985@gmail.com>
2023-11-19 13:50:15 +0200

    bug fix

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -40,9 +40,6 @@ class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
     String currentUserMail = _firebaseAuth.currentUser?.email ?? "";
-    // the user opened the chat so updating the unread messages num to zero
-    _chatService.updateUnreadMessagesCount(
-        currentUserMail, widget.receiverUserEmail, 0);
 
     return Scaffold(
       appBar: AppBar(
@@ -50,6 +47,9 @@ class _ChatPageState extends State<ChatPage> {
         leading: IconButton(
             icon: Icon(Icons.arrow_back),
             onPressed: () {
+              // the user opened the chat so updating the unread messages num to zero
+              _chatService.updateUnreadMessagesCount(
+                  currentUserMail, widget.receiverUserEmail, 0);
               _chatService.totalUnreadMessagesCount();
               Navigator.pop(context);
             }),
@@ -133,7 +133,8 @@ class _ChatPageState extends State<ChatPage> {
                         ? MainAxisAlignment.end
                         : MainAxisAlignment.start,
                 children: [
-                  Text('${timeOfMessage.toDate().hour}:${timeOfMessage.toDate().minute}'),
+                  Text(
+                      '${timeOfMessage.toDate().hour}:${timeOfMessage.toDate().minute}'),
                   const SizedBox(
                     width: 10,
                   ),
