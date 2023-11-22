67723f5efeab729d6c89c1d45c7d65818d53ac85
Yaniv-Tzipin <yaniv8985@gmail.com>
2023-11-22 14:52:15 +0200

    continue working on matches UI

--- lib/components/my_match_card.dart
+++ lib/components/my_match_card.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:myfirstapp/globals.dart';
 import 'package:myfirstapp/models/user.dart';
@@ -56,36 +57,50 @@ class CardContent extends StatefulWidget {
 }
 
 class _CardContentState extends State<CardContent> {
+  List<dynamic> potentialPartnerTags = currentUser.tags;
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
+        color: Colors.white,
           border: Border.all(color: Colors.black),
-          borderRadius: BorderRadius.circular(4)),
-      child: Column(
-        children: [
-          //image
-          ShadowOverlay(
-            shadowWidth: 350,
-            shadowHeight: 200,
-            child: buildImage(),
+          borderRadius: BorderRadius.circular(4),
           ),
-          //username + age
-          Align(
-            alignment: Alignment.topCenter,
-            child: Text(
-              '${widget.currentUser.username}, ${widget.currentUser.age}',
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            //image
+            ShadowOverlay(
+              shadowWidth: 350,
+              shadowHeight: 200,
+              child: buildImage(),
             ),
-          ),
-          //bio
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Align(
-              alignment: Alignment.topLeft,
-              child: aboutMe()),
-          )
-        ],
+            //username + age
+            Align(
+              alignment: Alignment.topCenter,
+              child: Text(
+                '${widget.currentUser.username}, ${widget.currentUser.age}',
+                style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+              ),
+            ),
+            //bio
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: aboutMe()),
+            ),
+            //my tags
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: myTagsBuilder(potentialPartnerTags)),
+            )
+
+            ///!!! to add my endoursments !!!\\\
+          ],
+        ),
       ),
     );
   }
@@ -103,9 +118,89 @@ class _CardContentState extends State<CardContent> {
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const Text('About me'),
-        Text(widget.currentUser.bio,)     
+        const Text('About me',
+        style: TextStyle(fontWeight: FontWeight.bold),),
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.white,
+            ),
+            borderRadius: BorderRadius.circular(8)),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(widget.currentUser.bio,overflow: TextOverflow.ellipsis,
+            maxLines: 3,),
+          ),
+        )     
       ],
     );
   }
+
+
+  Widget myTagsBuilder(potentialPartnerTags){
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+    return  Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('My tags',
+        style: TextStyle(fontWeight: FontWeight.bold),),
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.white,
+            ),
+            borderRadius: BorderRadius.circular(8)),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FutureBuilder<DocumentSnapshot>(
+              future: users.doc(FirebaseAuth.instance.currentUser?.email ?? "").get(),
+              builder: 
+              (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return const Text("Something went wrong");
+          }
+
+          if (snapshot.hasData && !snapshot.data!.exists) {
+            return const Text(
+                "Document does not exist");
+          }
+
+          if (snapshot.connectionState == ConnectionState.done) {
+            Map<String, dynamic> data =
+                snapshot.data!.data() as Map<String, dynamic>;
+            currentUser = UserProfile(data);
+            List<dynamic> currentUserTags = currentUser.tags;
+            return tagsCollection(potentialPartnerTags, currentUserTags);
+          }
+          return Container();})
+            ),
+          ),   
+      ],
+    );
+
+  }
+  Widget tagsCollection(List<dynamic> potentialPartnerTags, List<dynamic> currentUserTags){
+    List<Widget> ListOfTagsToDisplay = [];
+    for(dynamic tag in potentialPartnerTags){
+      if(currentUserTags.contains(tag)){
+        ListOfTagsToDisplay.add(InputChip(label: Text(tag),
+        isEnabled: false,
+        selected: true,
+        selectedColor: const Color.fromARGB(255, 233, 192, 148),
+        
+        ));
+      }
+      else{
+       ListOfTagsToDisplay.add(InputChip(label: Text(tag),
+       selected: false,
+       ));
+      }
+    }
+    return Wrap(
+      spacing: 3,
+      alignment: WrapAlignment.spaceEvenly,
+      children: ListOfTagsToDisplay,
+    );
+
+
+}
 }
--- lib/pages/matching_page.dart
+++ lib/pages/matching_page.dart
@@ -11,7 +11,9 @@ class MatchingBoard extends StatefulWidget {
 
 class _MatchingBoardState extends State<MatchingBoard> {
   final CardSwiperController swiperController = CardSwiperController();
-  final List<MyMatchCard> cards = [const MyMatchCard(cardRanking: 10, userEmail: 'yaniv8985@gmail.com')];
+  final List<MyMatchCard> cards = [const MyMatchCard(cardRanking: 10, userEmail: 'nadaveyal1@gmail.com'),
+  const MyMatchCard(cardRanking: 10, userEmail: 'yaniv8985@gmail.com')
+  ];
 
 
   @override
@@ -27,10 +29,57 @@ class _MatchingBoardState extends State<MatchingBoard> {
         child:Column(children: [
           Flexible(child: 
           CardSwiper(
+          controller: swiperController,
+          onSwipe: _onSwipe,
+          backCardOffset: const Offset(0, 0),
+          allowedSwipeDirection: AllowedSwipeDirection.only(down: false, up: false, right: true, left: true),
           cardBuilder: (context, index, horizontalThresholdPercentage, verticalThresholdPercentage) => cards[index],
           cardsCount: cards.length,
-          numberOfCardsDisplayed: cards.length,))
+          numberOfCardsDisplayed: cards.length,)),
+           Padding(
+             padding: const EdgeInsets.only(bottom:8.0),
+             child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+              IconButton(onPressed:swipeCardLeft, icon: const Icon(Icons.cancel),
+              iconSize: 60,
+              color: const Color.fromARGB(255, 209, 201, 194),),
+              const SizedBox(width: 70,),
+              IconButton(onPressed: swipeCardRight, icon: const Icon(Icons.favorite_rounded),
+              iconSize: 60,
+              color: const Color.fromARGB(255, 240, 119, 105),)
+             
+                       ],),
+           ),       
         ]) ,)
     );
   }
+
+ 
+  bool _onSwipe(
+    int previousIndex,
+    int? currentIndex,
+    CardSwiperDirection direction,
+  ) {
+    debugPrint(
+      'The card $previousIndex was swiped to the ${direction.name}. Now the card $currentIndex is on top',
+    );
+    return true;
+  }
+
+ 
+
+//it should be Future<void>
+void swipeCardLeft(){
+  swiperController.swipeLeft();
+
+}
+
+//it should be Future<void>
+void swipeCardRight(){
+  swiperController.swipeRight();
+
+}
+
 }
